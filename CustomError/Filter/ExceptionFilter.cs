72bae74b807cf7512b2c3caeb13cbe5240014ca8
yamikjg4 <yamikgandhi@outlook.com>
2022-03-03 11:11:05 +0530

    custom

---
+++ CustomError/Filter/ExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomError.Filter
+{
+    public class ExceptionFilter:ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            ViewResult results = new ViewResult();
+            results.ViewName = "/Views/Shared/ErrorMessage.cshtml";
+            results.ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(),new ModelStateDictionary());
+            results.ViewData.Model = context.Exception;
+            results.ViewData["Controller"] = context.RouteData.Values["controller"];
+            results.ViewData["Action"] = (string)context.RouteData.Values["action"];
+            context.Result = results;
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ CustomError/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomError.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
